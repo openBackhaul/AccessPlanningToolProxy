16e86415c44b599779f88bd07167b42fdc0b39fe
Lorenzo Latta <lorenzo.latta@siaemic.com>
2025-07-10 10:00:37 +0200

    Adding logger and debug mode for index.js and adiding launch config for vscode

--- server/index.js
+++ server/index.js
@@ -10,9 +10,18 @@ const logger = require('./service/LoggingService').getLogger();
 var serverPort = 4009;
 
 // uncomment if you do not want to validate security e.g. operation-key, basic auth, etc
-// TODO: check if is working fine
-//appCommons.openApiValidatorOptions.validateSecurity = false;
-//appCommons.openApiValidatorOptions.validateRequests = false;
+// appCommons.openApiValidatorOptions.validateSecurity = false;
+if (process.env.DEBUG && process.env.DEBUG.toLowerCase() === "true") {
+    logger.warn("Working in debug mode");
+    logger.warn("Checking validation");
+    appCommons.openApiValidatorOptions.validateSecurity = false;
+    // appCommons.openApiValidatorOptions.validateResponses = false;
+    // appCommons.openApiValidatorOptions.validateRequests = false;
+    logger.warn("Validate Security: " + appCommons.openApiValidatorOptions.validateSecurity);
+    logger.warn("Validate Responses: " + appCommons.openApiValidatorOptions.validateResponses);
+    logger.warn("Validate Requests: " + appCommons.openApiValidatorOptions.validateRequests);
+}
+
 
 // swaggerRouter configuration
 var options = {
@@ -34,6 +43,12 @@ http.createServer(app).listen(serverPort, function () {
 
 //setting the path to the database 
 global.databasePath = './database/config.json'
+if (process.env.DEBUG && process.env.DEBUG.toLowerCase() === "true") {
+    logger.warn("Working in debug mode");
+    global.databasePath = './server/database/config.json'
+    logger.warn("Load data from: " + global.databasePath);
+}
+
 // Limits
 global.counter = 0;
 global.counterStatus = 0;
@@ -44,4 +59,6 @@ global.counterStatusAcceptanceDataOfLinkEndpointCall = 0;
 //creating connected device list obj
 global.connectedDeviceList = "";
 global.testPrivateFunctions = 0;
+
+logger.info("Starting Application Registration");
 appCommons.performApplicationRegistration();
